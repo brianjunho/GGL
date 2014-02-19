c36c9c4eab7c098d0629dbde88a9623cc40ec346
Brian Lau <blths2k7@gmail.com>
2014-02-19 01:04:48 -0800

    Associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ActiveRecord::Base
 
 	validates :name, :prompt, :application, :option, presence: true
+	validates :option, length: {is: 1}
+	belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219080242) do
+ActiveRecord::Schema.define(version: 20140219083247) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140219080242) do
     t.string   "option"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
