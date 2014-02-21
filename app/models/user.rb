bc72736fb9e655e8001d9cd4aeefc76b517a83c4
Brian Lau <blths2k7@gmail.com>
2014-02-21 00:48:29 -0800

    remove dropbox image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ActiveRecord::Base
-	if Rails.env.development?
+	
     has_attached_file :avatar, :styles => { :medium => "200x200", :thumb => "200x200>" }, :default_url => "https://dl.dropboxusercontent.com/s/dg0tkzo002cvsf1/Default.png"
-  else
-    has_attached_file :avatar, :styles => { :medium => "200x200", :thumb => "200x200>" }, :default_url => "https://dl.dropboxusercontent.com/s/dg0tkzo002cvsf1/Default.png",
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"), :path => ":style/:id_:filename"
-	end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
