5b57b963f023852f78be39a02f815026087bb7ee
Brian Lau <blths2k7@gmail.com>
2014-03-21 00:48:04 -0700

    removed from schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,122 +36,8 @@ ActiveRecord::Schema.define(version: 20140321071333) do
     t.text     "four_suggestion"
   end
 
-  create_table "follows", force: true do |t|
-    t.string   "follower_type"
-    t.integer  "follower_id"
-    t.string   "followable_type"
-    t.integer  "followable_id"
-    t.datetime "created_at"
-  end
+  
 
-  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables"
-  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows"
-
-  create_table "forem_categories", force: true do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "slug"
-  end
-
-  add_index "forem_categories", ["slug"], name: "index_forem_categories_on_slug", unique: true
-
-  create_table "forem_forums", force: true do |t|
-    t.string  "name"
-    t.text    "description"
-    t.integer "category_id"
-    t.integer "views_count", default: 0
-    t.string  "slug"
-  end
-
-  add_index "forem_forums", ["slug"], name: "index_forem_forums_on_slug", unique: true
-
-  create_table "forem_groups", force: true do |t|
-    t.string "name"
-  end
-
-  add_index "forem_groups", ["name"], name: "index_forem_groups_on_name"
-
-  create_table "forem_memberships", force: true do |t|
-    t.integer "group_id"
-    t.integer "member_id"
-  end
-
-  add_index "forem_memberships", ["group_id"], name: "index_forem_memberships_on_group_id"
-
-  create_table "forem_moderator_groups", force: true do |t|
-    t.integer "forum_id"
-    t.integer "group_id"
-  end
-
-  add_index "forem_moderator_groups", ["forum_id"], name: "index_forem_moderator_groups_on_forum_id"
-
-  create_table "forem_posts", force: true do |t|
-    t.integer  "topic_id"
-    t.text     "text"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reply_to_id"
-    t.string   "state",       default: "pending_review"
-    t.boolean  "notified",    default: false
-  end
-
-  add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id"
-  add_index "forem_posts", ["state"], name: "index_forem_posts_on_state"
-  add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id"
-  add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id"
-
-  create_table "forem_subscriptions", force: true do |t|
-    t.integer "subscriber_id"
-    t.integer "topic_id"
-  end
-
-  create_table "forem_topics", force: true do |t|
-    t.integer  "forum_id"
-    t.integer  "user_id"
-    t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "locked",       default: false,            null: false
-    t.boolean  "pinned",       default: false
-    t.boolean  "hidden",       default: false
-    t.datetime "last_post_at"
-    t.string   "state",        default: "pending_review"
-    t.integer  "views_count",  default: 0
-    t.string   "slug"
-  end
-
-  add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id"
-  add_index "forem_topics", ["slug"], name: "index_forem_topics_on_slug", unique: true
-  add_index "forem_topics", ["state"], name: "index_forem_topics_on_state"
-  add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id"
-
-  create_table "forem_views", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "viewable_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "count",             default: 0
-    t.string   "viewable_type"
-    t.datetime "current_viewed_at"
-    t.datetime "past_viewed_at"
-  end
-
-  add_index "forem_views", ["updated_at"], name: "index_forem_views_on_updated_at"
-  add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id"
-  add_index "forem_views", ["viewable_id"], name: "index_forem_views_on_viewable_id"
-
-  create_table "likes", force: true do |t|
-    t.string   "liker_type"
-    t.integer  "liker_id"
-    t.string   "likeable_type"
-    t.integer  "likeable_id"
-    t.datetime "created_at"
-  end
-
-  add_index "likes", ["likeable_id", "likeable_type"], name: "fk_likeables"
-  add_index "likes", ["liker_id", "liker_type"], name: "fk_likes"
 
   create_table "listings", force: true do |t|
     t.text     "prompt"
@@ -165,16 +51,8 @@ ActiveRecord::Schema.define(version: 20140321071333) do
     t.text     "comments"
   end
 
-  create_table "mentions", force: true do |t|
-    t.string   "mentioner_type"
-    t.integer  "mentioner_id"
-    t.string   "mentionable_type"
-    t.integer  "mentionable_id"
-    t.datetime "created_at"
-  end
+  
 
-  add_index "mentions", ["mentionable_id", "mentionable_type"], name: "fk_mentionables"
-  add_index "mentions", ["mentioner_id", "mentioner_type"], name: "fk_mentions"
 
   create_table "orders", force: true do |t|
     t.datetime "created_at"
@@ -199,24 +77,8 @@ ActiveRecord::Schema.define(version: 20140321071333) do
     t.integer  "edit_id"
   end
 
-  create_table "relistings", force: true do |t|
-    t.text     "application"
-    t.text     "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "requester_id"
-    t.integer  "editor_id"
-    t.integer  "edit_id"
-  end
-
-  create_table "reviews", force: true do |t|
-    t.decimal  "rating"
-    t.text     "statement"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reviewee_id"
-    t.integer  "reviewer_id"
-  end
+  
+ 
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",               null: false
